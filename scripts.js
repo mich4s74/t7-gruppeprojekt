15696d95d6b17c7813d4a8a557ab797f214f5a03
“isab554b” <isab554b@stud.kea.dk>
2021-09-09 14:21:41 +0200

    json

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-console.log("hello world!");
+const url = "";
+
+const options = {
+  headers: {
+    "x-apikey": "",
+  },
+};
+
+async function hentData() {
+  const respons = await fetch(url, options);
+  const json = await respons.json();
+  vis(json);
+}
+
+function vis(json) {
+  console.log(json);
+}
+hentData();
